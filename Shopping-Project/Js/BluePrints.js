b2a78568a927beb35e5502ad38ed99fe6065b605
-Mim <cod16duty@gmail.com>
2024-04-07 04:36:19 +0330

    VS 0.002

--- Shopping-Project/Js/BluePrints.js
+++ Shopping-Project/Js/BluePrints.js
@@ -1,7 +1,9 @@
 class Products {
-  constructor(SectionParent, Data) {
+  constructor(SectionParent, Data, Cart) {
     this.CardPlace = SectionParent;
     this.ProductData = Data;
+    this.Cart = Cart;
+    this.CardPlace.addEventListener("click", this);
   }
   showProduct() {
     this.ProductData.forEach((Product) => this.CreateProduct(Product));
@@ -49,7 +51,7 @@ class Products {
     const { id } = Product;
     const AddButton = document.createElement("button");
     AddButton.innerText = "+";
-    AddButton.setAttribute("data-set", id);
+    AddButton.setAttribute("data-id", id);
     return AddButton;
   }
   ApplyTime(CardProduct, ProductInfo, ProPrBu, ProductPrice, AddButton) {
@@ -58,5 +60,73 @@ class Products {
     CardProduct.appendChild(ProductInfo);
     this.CardPlace.appendChild(CardProduct);
   }
+  handleEvent() {
+    const eve = event.target;
+    if (eve.tagName === "BUTTON") {
+      this.addToCart(eve.dataset.id);
+    }
+  }
+  addToCart(id) {
+    const Pro = this.ProductData.find((i) => i.id === +id);
+    this.Cart.ProductBoughtList.push(Pro);
+    this.Cart.ShowBoughtProduct();
+  }
 }
-export default Products;
+class Cart {
+  constructor(ListOfProductsPlace, PriceOfProducts) {
+    this.ListOfProductsPlace = ListOfProductsPlace;
+    this.PriceOfProducts = PriceOfProducts;
+    this.ProductBoughtList = [];
+    this.FinalProductBoughtList = [];
+  }
+  ShowBoughtProduct() {
+    this.FinalProductBoughtList = [...new Set(this.ProductBoughtList)];
+    this.ListOfProductsPlace.innerHTML = "";
+    this.FinalProductBoughtList.forEach((Product) => {
+      const Quantity = this.ProductBoughtList.filter(
+        (P) => P.id === Product.id
+      ).length;
+      this.CreateBoughtProductSection(Product, Quantity);
+    });
+  }
+  CreateBoughtProductSection(Product, Quantity) {
+    const CardEle = document.createElement("div");
+    CardEle.innerHTML = this.CreateImgEle(Product);
+    CardEle.innerHTML += this.CreateInfoEle(Product);
+    CardEle.innerHTML += this.CreateControlEle(Product, Quantity);
+    this.ListOfProductsPlace.appendChild(CardEle);
+    console.log(Product);
+  }
+  CreateImgEle(data) {
+    const { image, alt } = data;
+    const ImageJSX = `
+      <img src = ${image} alt = ${alt}></img>
+    `;
+    return ImageJSX;
+  }
+  CreateInfoEle(data) {
+    const { name, price } = data;
+    const InfoJSX = `
+      <div>
+        <h3>${name}</h3>
+        <p>${price} $</p>
+      </div>
+    `;
+    return InfoJSX;
+  }
+  CreateControlEle(data, Quantity) {
+    const { id } = data;
+    const ControlJSX = `
+      <div>
+        <div>
+        <button data-id = ${id} class="custom-btn btn-addremove"><span>-</span></button>
+        <span>${Quantity}</span>
+        <button data-id = ${id} class="custom-btn btn-addremove"><span>+</span></button>
+        </div>
+        <button class = "explore" data-id = ${id}>Remove</button>
+      </div>
+    `;
+    return ControlJSX;
+  }
+}
+export { Products as Products, Cart as Carting };
