ec295985e95d4cf265910c879f4ed80cf3f2eee9
-Mim <cod16duty@gmail.com>
2024-04-07 04:36:32 +0330

    VS 0.002

--- Shopping-Project/Js/Logic.js
+++ Shopping-Project/Js/Logic.js
@@ -1,10 +1,15 @@
 import Getting from "./RequestData.js";
-import Products from "./BluePrints.js";
+import { Products, Carting } from "./BluePrints.js";
 const ProductSection = document.getElementById("Products");
+const PriceCart = document.getElementById("PriceCart");
+const ListCart = document.getElementById("List");
 document.addEventListener("DOMContentLoaded", load);
 async function load() {
   const Getto = await Getting();
-  const ProductInstance = new Products(ProductSection, Getto);
+  const CartInstance = new Carting(ListCart, PriceCart);
+  const ProductInstance = new Products(ProductSection, Getto, CartInstance);
+  console.log(CartInstance);
   console.log(ProductInstance);
   ProductInstance.showProduct();
+  CartInstance.ShowBoughtProduct();
 }
